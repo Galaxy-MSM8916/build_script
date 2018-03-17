28d2c301e06399fcf5eff15a22c39f15b23d18db
Vincent Zvikaramba <zvikovincent@gmail.com>
2018-03-17 14:29:57 -0400

    Add initial build script
    Change-Id: I0430dfba38ead381c8295cc77984926b71c17ade

---
+++ build.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import getopt
+import os
+import sys
+
+import conf
+
+file_name = os.path.basename(sys.argv[0])
+
+def usage():
+    print("Usage: {} -t <target> [OPTIONS]".format(file_name))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        usage()
